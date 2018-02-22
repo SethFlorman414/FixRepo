d0f69b7c24934c6553d244be4a9d3daf14a943b3
Seth Florman <18.seth.florman@ksd.org>
2018-02-22 13:49:27 -0800

    Making Final Room. SF.

---
+++ Assets/Scripts/BaseDoor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseDoor : MonoBehaviour {
+
+    // Use this for initialization
+
+    public Animator anim;
+
+	void Start () {
+
+        
+        
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+
+
+	}
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+
+        
+        if(other.gameObject.tag == "Player")
+        {
+           // Debug.Log("player");
+            anim.SetBool("playerTrigger", true);
+            
+
+        }
+
+
+    }
+
+}
