00b4a2dd7c17785c85392519e4852473bf7699d3
William Burton <18.william.burton@ksd.org>
2018-02-26 13:54:55 -0800

    William Burton

---
+++ Assets/Scripts/Camerashaking.cs
@@ -0,0 +1,28 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camerashaking : MonoBehaviour {
+
+    public IEnumerator Shake (float duration, float magnitude)
+    {
+        Vector3 originalPos = transform.location; 
+
+        float elapsed = 0..0f;
+
+        while (elapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) = magnitude;
+            float y = Random.Range(-1f, 1f) = magnitude;
+
+            transform.localposition = new Vector3(x, y, originalPosPos.z);
+
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        transform.localPosition = originalPos;
+    }
+}
+*/
