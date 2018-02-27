4b7a8f7015296d353581845fc2e11d52afb4407b
Seth Florman <18.seth.florman@ksd.org>
2018-02-27 13:49:48 -0800

    level design. SF

---
+++ Assets/Scripts/DIfficulties.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DIfficulties : MonoBehaviour {
+
+    // Use this for initialization
+
+    public GameObject light;
+    public GameObject gameLight1;
+    public GameObject gameLight2;
+    public GameObject gameLight3;
+   
+
+    public GameObject health1;
+    public GameObject health2;
+    public GameObject toolBench;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+    public void OnButtonEasy()
+    {
+
+
+        PlayerPrefs.SetInt("Toxins", PlayerPrefs.GetInt("Toxins") + 0);
+
+
+    }
+
+
+
+    public void OnButtonMedium()
+    {
+
+        PlayerPrefs.SetInt("Toxins", PlayerPrefs.GetInt("Toxins") + 250);
+        light.SetActive(false);
+
+    }
+
+
+    public void OnButtonHard()
+    {
+
+        PlayerPrefs.SetInt("Toxins", PlayerPrefs.GetInt("Toxins") + 500);
+        light.SetActive(false);
+        health1.SetActive(false);
+        health2.SetActive(false);
+        gameLight1.SetActive(false);
+        gameLight2.SetActive(false);
+        gameLight3.SetActive(false);
+
+        toolBench.SetActive(true);
+
+
+    }
+
+}
---
+++ Assets/Scripts/SpawnLight.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnLight : MonoBehaviour {
+
+    // Use this for initialization
+
+    public GameObject light;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void OnTriggerExit(Collider collision)
+    {
+        
+
+        if(collision.gameObject.tag == "Flashlight")
+        {
+
+            light.SetActive(true);
+
+        }
+
+    }
+
+}
