4338408cb066a2b25c31f132ead0c370bd06ef69
Seth Florman <18.seth.florman@ksd.org>
2018-02-28 13:49:33 -0800

    More Level Designing. SF

---
+++ Assets/needTimer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class needTimer : MonoBehaviour {
+
+    // Use this for initialization
+
+    public float newTimer;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        newTimer += Time.deltaTime;
+
+        if(newTimer >= 5.0f)
+        {
+
+            SceneManager.LoadScene("Start Menu");
+
+        }
+
+		
+	}
+}
