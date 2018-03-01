8f39c4ecd5900fc29462a5a8bb2807501c0b8a1d
William Burton <18.william.burton@ksd.org>
2018-03-01 13:02:43 -0800

    Heree we go.

--- Assets/Scripts/exit.cs
+++ Assets/Scripts/exit.cs
@@ -1,14 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class exit : MonoBehaviour {
 
 	// Use this for initialization
      void Update() {
-        OnButtonClick(){
-            Debug.Log("It works now");
-            Application.Quit();
-
+      
              }
-		 }
+		 
+    public void OnButtonClick()
+    {
+        Debug.Log("It works now");
+
+
+
+    }
+
+
+
 	}
